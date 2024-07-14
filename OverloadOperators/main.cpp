c1c60b0042603641504eaab7b83df36e6f67302c
ivona <ik87056@student.uwb.edu.pl>
2024-07-14 23:55:21 +0200

    added CMakeList and main.cpp

---
+++ OverloadOperators/main.cpp
@@ -0,0 +1,33 @@
+#include <locale>
+#include <iostream>
+#include <tchar.h>
+
+using namespace std;
+
+enum SERVER {
+    STARTING, SHOOTDOWN, WORK, STOP
+};
+enum CLIENT {
+    CONNECT, SEND, RECV, DISCONNECT
+};
+enum EVENT {
+    WAIT, DO, ERROR
+};
+
+EVENT operator>>(CLIENT c, SERVER s) {
+    EVENT rc = ERROR;
+    if ((c == CONNECT || c == SEND || c == RECV) && (s == STARTING || s == STOP)) rc = WAIT;
+    else if ((c == CONNECT || c == SEND || c == RECV) && (s == WORK)) rc = DO;
+    return rc;
+};
+
+int _tmain(int argc, _TCHAR *argv[]) {
+
+    EVENT e1 = CONNECT >> STARTING;
+    EVENT e2 = CONNECT >> WORK;
+    EVENT e3 = SEND >> SHOOTDOWN;
+
+    system("pause");
+    return 0;
+}
+
